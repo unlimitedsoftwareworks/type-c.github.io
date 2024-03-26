00949e86149bc4323668e5eec6e208d321997162
praisethemoon <doit@praisethemoon.org>
2024-03-26 23:55:35 +0100

    fix build2

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   // Optionally, add any other Next.js config below
 }
  
