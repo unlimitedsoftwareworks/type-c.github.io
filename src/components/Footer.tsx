9394c1c857a7b8b76eb8583053693756cd5e26bb
praisethemoon <doit@praisethemoon.org>
2024-03-26 01:08:01 +0100

    updated contact section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
     return (
         <footer className="footer items-center p-4 bg-neutral text-neutral-content w-full z-50">
             <aside className="items-center grid-flow-col">
-                <p>© 2024 - Soulaymen Chouri (praisethemoon). Type-C's documentation is licensed under CC-BY-3.0</p>
+                <p>© 2024 - Soulaymen Chouri (praisethemoon). Type-C's documentation is licensed under CC-BY-3.0; For any contact request, please send your inquiery to <Link href="mailto:doit@praisethemoon.org">doit@praisethemoon.org</Link></p>
             </aside>
             <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
                 <Link className="navbar-end" href="https://github.com/unlimitedsoftwareworks/type-c"><FaGithub size={24} /></Link>
