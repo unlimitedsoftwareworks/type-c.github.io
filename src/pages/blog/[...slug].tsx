79cecb04df46024a0b5f09d1f6baf3abea3d51f4
praisethemoon <doit@praisethemoon.org>
2024-09-17 23:21:51 +0200

    ++

--- src/pages/blog/[...slug].tsx
+++ src/pages/blog/[...slug].tsx
@@ -1,50 +1,56 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { serialize } from 'next-mdx-remote/serialize';
-import { MDXRemote } from 'next-mdx-remote';
-import fs from 'fs';
-import path from 'path';
-import { BlogPost } from '../posts';
-import MDXComponents from '@/components/MDXComponents';
-import Link from 'next/link';
-import { FaDochub, FaGithub, FaGlobeAfrica, FaNewspaper, FaRegNewspaper } from 'react-icons/fa';
-import { HiOutlineDocumentText } from 'react-icons/hi';
-import { useEffect, useRef } from 'react';
-import Image from 'next/image';
+import { GetStaticPaths, GetStaticProps } from "next";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import fs from "fs";
+import path from "path";
+import { BlogPost } from "../posts";
+import MDXComponents from "@/components/MDXComponents";
+import Link from "next/link";
+import {
+    FaDochub,
+    FaGithub,
+    FaGlobeAfrica,
+    FaNewspaper,
+    FaRegNewspaper,
+} from "react-icons/fa";
+import { HiOutlineDocumentText } from "react-icons/hi";
+import { useEffect, useRef } from "react";
+import Image from "next/image";
 
 // Define the path to your posts directory
-const postsDirectory = path.join(process.cwd(), 'posts');
+const postsDirectory = path.join(process.cwd(), "posts");
 
 const UtterancesComments = () => {
     const ref = useRef<HTMLDivElement>(null);
-  
+
     useEffect(() => {
-      const script = document.createElement('script');
-  
-      const config = {
-        src: 'https://utteranc.es/client.js',
-        repo: 'unlimitedsoftwareworks/type-c.io-conversation',
-        'issue-term': 'pathname',
-        theme: 'github-light',
-        crossOrigin: 'anonymous',
-        defer: true,
-      };
-  
-      Object.entries(config).forEach(([key, value]) => {
-        script.setAttribute(key, value as string);
-      });
-  
-      setTimeout(() => {
-        ref.current?.append(script);
-      }, 300);
+        const script = document.createElement("script");
+
+        const config = {
+            src: "https://utteranc.es/client.js",
+            repo: "unlimitedsoftwareworks/type-c.io-conversation",
+            "issue-term": "pathname",
+            theme: "github-light",
+            crossOrigin: "anonymous",
+            defer: true,
+        };
+
+        Object.entries(config).forEach(([key, value]) => {
+            script.setAttribute(key, value as string);
+        });
+
+        setTimeout(() => {
+            ref.current?.append(script);
+        }, 300);
     }, []);
-  
+
     return <div ref={ref} />;
-  };
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
     // Read the _meta.json file to get the slugs
-    const metaFilePath = path.join(postsDirectory, '_meta.json');
-    const jsonData = fs.readFileSync(metaFilePath, 'utf8');
+    const metaFilePath = path.join(postsDirectory, "_meta.json");
+    const jsonData = fs.readFileSync(metaFilePath, "utf8");
     const posts = JSON.parse(jsonData).posts;
 
     // Generate the paths for getStaticPaths
@@ -59,21 +65,22 @@ export const getStaticProps: GetStaticProps = async (context) => {
     // @ts-ignore
     const slug = context.params.slug[0];
     const mdxPath = path.join(postsDirectory, `${slug}.mdx`);
-    const source = fs.readFileSync(mdxPath, 'utf8');
+    const source = fs.readFileSync(mdxPath, "utf8");
 
     // Load the meta file
-    const metaFilePath = path.join(postsDirectory, '_meta.json');
-    const jsonData = fs.readFileSync(metaFilePath, 'utf8');
+    const metaFilePath = path.join(postsDirectory, "_meta.json");
+    const jsonData = fs.readFileSync(metaFilePath, "utf8");
     const content = JSON.parse(jsonData);
     const posts = content.posts;
     const authors = content.authors;
 
     // Find the metadata for the current post
     const postMeta = posts.find((post: BlogPost) => post.name === slug);
-    const authorMeta = authors.find((author: any) => author.id === postMeta.author);
+    const authorMeta = authors.find(
+        (author: any) => author.id === postMeta.author,
+    );
 
     postMeta.author = authorMeta;
-
 
     // Use MDXRemote to serialize the MDX content
     const mdxSource = await serialize(source);
@@ -92,24 +99,31 @@ const PostPage = ({ source, meta }: any) => {
             <div className="striped1 hero bg-base-100 py-20">
                 <div>
                     <div className="hero-content text-center">
-                        <div className="gap-x-40 text-center flex flex-col text-center  items-center">
+                        <div className="gap-x-40 flex flex-col text-center  items-center">
                             <p className="text-5xl">{meta.title}</p>
-                            <p className='m-1'>{meta.date}</p>
+                            <p className="m-1">{meta.date}</p>
                             <div className="flex flex-row gap-x-2">
                                 {meta.tags.map((tag: string) => (
-                                    <div key={tag} className="badge badge-neutral">{tag}</div>
+                                    <div
+                                        key={tag}
+                                        className="badge badge-neutral"
+                                    >
+                                        {tag}
+                                    </div>
                                 ))}
                             </div>
                             <div className="text-sm breadcrumbs">
                                 <ul>
                                     <li>
                                         <Link href="/posts">
-                                            <FaRegNewspaper className='mx-2' /> Blog
+                                            <FaRegNewspaper className="mx-2" />{" "}
+                                            Blog
                                         </Link>
                                     </li>
                                     <li>
                                         <a>
-                                            <HiOutlineDocumentText className='mx-2' />{meta.title}
+                                            <HiOutlineDocumentText className="mx-2" />
+                                            {meta.title}
                                         </a>
                                     </li>
                                 </ul>
@@ -117,7 +131,6 @@ const PostPage = ({ source, meta }: any) => {
                         </div>
                     </div>
                 </div>
-
             </div>
 
             <div className="hero bg-base-200">
@@ -130,33 +143,51 @@ const PostPage = ({ source, meta }: any) => {
 
             <div className="striped1 hero bg-base-100 py-20">
                 <div className="py-1 w-700">
-                    <div className='prose lg:prose-md m-auto b-1'>
+                    <div className="prose lg:prose-md m-auto b-1">
                         <h2>Written By</h2>
                     </div>
                     <div className="stat bg-base-100 hover:bg-primary hover:text-blue-500">
                         <div className="stat-figure text-secondary">
                             <div className="avatar">
                                 <div className="w-16 rounded-full">
-                                    <Image alt="" src={meta.author.avatar} width={128} height={128}/>
+                                    <Image
+                                        alt=""
+                                        src={meta.author.avatar}
+                                        width={128}
+                                        height={128}
+                                    />
                                 </div>
                             </div>
                         </div>
-                        <div className='prose'>
+                        <div className="prose">
                             <div className="stat-value ">{meta.author.id}</div>
                             <div className="stat-title">{meta.author.name}</div>
-                            <div className="stat-desc text-secondary">{meta.author.bio}</div>
-                            <div className='flex flex-1 flex-row'>
-                                <Link href={meta.author.website} className='btn btn-ghost px-5 btn-xs'><FaGlobeAfrica /></Link> <Link className='btn btn-ghost px-5 btn-xs' href={meta.author.gh}><FaGithub /></Link>
+                            <div className="stat-desc text-secondary">
+                                {meta.author.bio}
+                            </div>
+                            <div className="flex flex-1 flex-row">
+                                <Link
+                                    href={meta.author.website}
+                                    className="btn btn-ghost px-5 btn-xs"
+                                >
+                                    <FaGlobeAfrica />
+                                </Link>{" "}
+                                <Link
+                                    className="btn btn-ghost px-5 btn-xs"
+                                    href={meta.author.gh}
+                                >
+                                    <FaGithub />
+                                </Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='bg-base-200 min-h-[400px]'>
-                <UtterancesComments/>
+            <div className="bg-base-200 min-h-[400px]">
+                <UtterancesComments />
             </div>
         </div>
-    )
+    );
 };
 
 export default PostPage;
