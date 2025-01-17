d3fc82ca59fab8e1b24ed3db009146be38f3f5ac
praisethemoon <doit@praisethemoon.org>
2025-01-17 17:10:51 +0100

    footer++

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,26 +5,16 @@ import { FaGithub } from "react-icons/fa";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="footer items-center p-4 bg-base-100 text-neutral-content w-full z-50">
+        <footer className="footer items-center p-4 bg-base-200 primary-content w-full z-50">
             <aside>
                 <p>
-                    © 2024 - Soulaymen Chouri (praisethemoon). Type-C&apos;s
+                    © 2025 - Soulaymen Chouri (praisethemoon). Type-C&apos;s
                     documentation is licensed under CC-BY-3.0; Contact:{" "}
                     <Link href="mailto:doit@praisethemoon.org">
                         doit@praisethemoon.org
                     </Link>
                 </p>
             </aside>
-            <nav>
-                <div className="grid grid-flow-col gap-4">
-                    <Link
-                        className="navbar-end"
-                        href="https://github.com/unlimitedsoftwareworks/type-c"
-                    >
-                        <FaGithub size={24} />
-                    </Link>
-                </div>
-            </nav>
         </footer>
     );
 };
